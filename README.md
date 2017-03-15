d9f75dba4a1c6f79ec6129336aa17be7dcacdb08
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-03-15 21:45:40 +0100

    Commit initial, scripts pour conversion mfcc anglais

---
+++ README.md
@@ -0,0 +1,2 @@
+Nécessite python3.
+convertToMfcc audio transcript confightk transforme le fichier en .mfcc.
---
+++ scripts/convertToMfcc.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from subprocess import call
+
+audioFileName = sys.argv[1]
+transFileName = sys.argv[2]
+
+#Conversion en wav si besoin
+if (os.path.splitext(audioFileName)[1] != ".wav") :
+    wavFileName = os.path.splitext(audioFileName)[0] + ".wav"
+    call("sox " + audioFileName + " " + wavFileName, shell = True)
+    audioFileName = wavFileName
+
+#Coupe de l'audio
+cutFileName = os.path.splitext(audioFileName)[0] + "_trimmed.wav"
+call("python3 cuttingEnglish.py " + transFileName + " " + audioFileName + " " + cutFileName, shell = True) 
+
+#Génération des mfcc
+configFile = sys.argv[3]
+mfcFileName = os.path.splitext(audioFileName)[0] + ".mfcc"
+call("./HCopy -C " + configFile + " " + cutFileName + " " + mfcFileName, shell = True) 
