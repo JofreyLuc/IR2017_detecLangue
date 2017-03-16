a203a2eb5248c704c98c80bbe6e911e05784bba5
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-03-16 18:30:02 +0100

    config htk b/l endian

--- scripts/convertToMfcc.py
+++ scripts/convertToMfcc.py
@@ -3,30 +3,32 @@
 #python3 audio.* transcript.stm confightk
 
 import cutStm               #Script de découpe des stm
+import cutMlfmanu           #Script de découpe des mlfmanu
 from os import path         #Pour couper l'extension de fichier
 from os import remove       #Pour supprimer des fichiers
 import sys                  #Pour récuperer les arguments du programme
 from subprocess import call #Pour lancer sox et htk
 from platform import system #Pour connaitre l'environnement d'execution
 
-hasBeenConverted = False    #Indique si on a du convertir le fichier en .wav
-
-#Gestion de l'OS
-linux=False
-windows=False
-if (system() == "Linux") :
-    linux = True
-elif (system() == 'Windows') :
-    windows = True
-else :
-    sys.exit("OS inconnu")
 
 def convertToMfcc(audioFileName, transFileName, configFile) :
     
+    #Gestion de l'OS
+    linux=False
+    windows=False
+    if (system() == "Linux") :
+        linux = True
+    elif (system() == "Windows") :
+        windows = True
+    else :
+        sys.exit("OS inconnu")
+
+    hasBeenConverted = False    #Indique si on a du convertir le fichier en .wav
+        
     audioName = path.splitext(path.basename(audioFileName))[0] #Nom du fichier audio sans extension
     
     #Conversion en wav si besoin
-    if (not(path.splitext(audioFileName)[1] is ".wav")) :
+    if (path.splitext(audioFileName)[1] != ".wav") :
         wavFileName = audioName + ".wav"
         call("sox " + audioFileName + " " + wavFileName, shell = True)
         audioName = wavFileName
@@ -34,7 +36,12 @@ def convertToMfcc(audioFileName, transFileName, configFile) :
 
     #Coupe de l'audio
     cutFileName = path.splitext(audioName)[0] + "_trimmed.wav"
-    cutStm.cutStm(audioName, transFileName, cutFileName)
+    if (path.splitext(transFileName)[1] == ".stm") :
+        cutStm.cutStm(audioFileName, transFileName, cutFileName)
+    elif (path.splitext(transFileName)[1] == ".mlfmanu") :
+        cutMlfmanu.cutMlfmanu(audioFileName, transFileName, cutFileName)
+    else :
+        sys.exit("Format de transcript inconnu")
 
     #Génération des mfcc
     mfcFileName = path.splitext(audioName)[0] + ".mfcc"
