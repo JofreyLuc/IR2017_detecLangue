4845bd36ee29b2afcb457086c396775e887847cb
Zhephyr54 <ale.lanoix@gmail.com>
2017-03-20 16:39:26 +0100

    convertMfccToCSV

--- convertMfccToCSV.py
+++ convertMfccToCSV.py
@@ -2,8 +2,13 @@ import sys,os
 from subprocess import call
 from platform import system             #Pour connaitre l'environnement d'execution
 import csv
+from os import remove                   #Pour supprimer des fichiers
+
+tmpFile = "HListTmp"
 
 mfccFileName = sys.argv[1]
+language = sys.argv[2]
+outputFile = sys.argv[3]
 
 #Gestion de l'OS
 linux=False
@@ -20,16 +25,21 @@ if (linux) :
     hListCall = "./HList"
 elif (windows) :
     hListCall = "HList"
-call(hListCall + " " + mfccFileName + " >> test.txt", shell = True)        
+call(hListCall + " " + mfccFileName + " >> " + tmpFile, shell = True)        
 
-ifile = open("test.txt", 'r') 
-ofile = open("sortie.txt", 'a', newline="")
+ifile = open(tmpFile, 'r') 
+ofile = open(outputFile, 'a', newline="")
 wr = csv.writer(ofile)
 currentLine = ifile.readline()
 while (currentLine != '') :
     if (not currentLine.startswith('-')) :
         currentLine += ifile.readline()
         mfcc = currentLine.split()[1:]
+        mfcc.append(language)
         wr.writerow(mfcc)
-        
+   
     currentLine = ifile.readline()
+
+ifile.close()
+ofile.close()    
+remove(tmpFile)
