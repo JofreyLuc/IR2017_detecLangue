4b78b8d0a7896dd88107305751a7dae1193f46d6
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-03-20 17:44:24 +0100

    launchkeras + 0123

--- scripts/launchKeras.py
+++ scripts/launchKeras.py
@@ -1,9 +1,11 @@
 # Create first network with Keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils.np_utils import to_categorical
 import numpy
+import sys
 
-dataFile = sys.argv["1"]
+dataFile = sys.argv[1]
 
 # fix random seed for reproducibility
 seed = 7
@@ -13,16 +15,17 @@ dataset = numpy.loadtxt(dataFile, delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:13]
 Y = dataset[:,13]
+Y = to_categorical(Y)
 # create model
 model = Sequential()
-model.add(Dense(390, input_dim=13, init='uniform', activation='relu'))
-model.add(Dense(256, init='uniform', activation='relu'))
-model.add(Dense(256, init='uniform', activation='relu'))
-model.add(Dense(4, init='uniform', activation='sigmoid'))
+model.add(Dense(390, input_dim=13, kernel_initializer='normal', activation='relu'))
+model.add(Dense(256, kernel_initializer='normal', activation='relu'))
+model.add(Dense(256, kernel_initializer='normal', activation='relu'))
+model.add(Dense(4, kernel_initializer='normal', activation='sigmoid'))
 # Compile model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
-model.fit(X, Y, nb_epoch=150, batch_size=10)
+model.fit(X, Y, epochs=150, batch_size=10)
 # evaluate the model
 #scores = model.evaluate(X, Y)
 #print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
