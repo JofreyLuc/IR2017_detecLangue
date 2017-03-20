c00e2d6de6eabc73d992b147f0d79077d33aece4
Zhephyr54 <ale.lanoix@gmail.com>
2017-03-20 17:44:47 +0100

    lol

--- scripts/formatCsv.py
+++ scripts/formatCsv.py
@@ -6,22 +6,21 @@ from glob import glob
 
 def formatCsv(mfccFolderName, csvFileName) :
 
-    AFFICHAGE = True
-
+    AFFICHAGE = False
     for arabicFile in glob(mfccFolderName + "arabic/*.mfcc") :
-        check_call([sys.executable, "convertMfccToCSV.py", arabicFile, str(1), csvFileName], shell=False)
+        check_call([sys.executable, "convertMfccToCSV.py", arabicFile, str(0), csvFileName], shell=False)
         if AFFICHAGE : print("Traité : " + arabicFile)
 
     for englishFile in glob(mfccFolderName + "english/*.mfcc") :
-        check_call([sys.executable, "convertMfccToCSV.py", englishFile, str(2), csvFileName], shell=False)
+        check_call([sys.executable, "convertMfccToCSV.py", englishFile, str(1), csvFileName], shell=False)
         if AFFICHAGE : print("Traité : " + englishFile)
 
     for frenchFile in glob(mfccFolderName + "french/*.mfcc") :
-        check_call([sys.executable, "convertMfccToCSV.py", frenchFile, str(3), csvFileName], shell=False)
+        check_call([sys.executable, "convertMfccToCSV.py", frenchFile, str(2), csvFileName], shell=False)
         if AFFICHAGE : print("Traité : " + frenchFile)
 
     for germanFile in glob(mfccFolderName + "german/*.mfcc") :
-        check_call([sys.executable, "convertMfccToCSV.py", germanFile, str(4), csvFileName], shell=False)
+        check_call([sys.executable, "convertMfccToCSV.py", germanFile, str(3), csvFileName], shell=False)
         if AFFICHAGE : print("Traité : " + germanFile)
     
 if __name__ == '__main__':
