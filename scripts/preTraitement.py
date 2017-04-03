66bf6becb40f80ec2c2486c8911b7d91fbfba101
Jofrey Luc <luc.jofrey@yahoo.fr>
2017-04-03 15:17:28 +0200

    pretraitement hdf5

--- scripts/preTraitement.py
+++ scripts/preTraitement.py
@@ -12,8 +12,8 @@ def preTraitement() :
     if AFFICHAGE : print("Conversion de l'audio en mfcc..")
     check_call([sys.executable, "convertAllToMfcc.py"], shell=False)
 
-    if AFFICHAGE : print("Conversion des mfcc en csv..")
-    check_call([sys.executable, "convertAllToCsv.py", "../mfcc/", "data.csv"], shell=False)
+    if AFFICHAGE : print("Conversion des mfcc en hdf5..")
+    check_call([sys.executable, "convertAllToHdf5.py", "../mfcc/", "data.hdf5"], shell=False)
 
 if __name__ == '__main__':
     preTraitement()
