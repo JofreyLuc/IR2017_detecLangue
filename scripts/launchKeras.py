f1ba5ee5dfeb78fe16d70641d9f156f309cbbd64
Zhephyr54 <ale.lanoix@gmail.com>
2017-04-04 20:38:42 +0200

    Refactor et commentaires launchKeras

--- scripts/launchKeras.py
+++ scripts/launchKeras.py
@@ -6,78 +6,86 @@ from keras.utils.np_utils import to_categorical
 import numpy as np
 import h5py
 import sys
+from os import remove                   #Pour supprimer des fichiers
 
-#On récupère les données
+# Paramètres
+# Nombre de coefficients cepstraux
+nbCoef = 13
+# Nombre de valeurs à prélever pour obtenir une "fenêtre de parole"
+nbVal = 31
+# Décalage entre chaque prélevement de fenêtre de parole
+shift = 10
+
+# On récupère les données
 dataFile = sys.argv[1]
 hdf5In = h5py.File(dataFile, "r")
 
-#création d'un fichier hdf5 temporaire
-hdf5Out = h5py.File("tmp.hdf5", "w")
-
-#Nombre d'exemples
+# Calcul du nombre total de fenêtres de parole de la base d'apprentissage
+# Une fenêtre est une matrice contenant des coefficients cepstraux :
+# - de nbVal lignes 
+# - de nbCoef colonnes
 totalFrames = 0
+for dataset in hdf5In.values():                             # Pour chaque dataset du fichier hdf5
+    frames = int ((dataset.shape[0] - nbVal) / shift) + 1   # Nb fenêtres du dataset
+    totalFrames += frames
+    
+# Création d'un fichier hdf5 temporaire afin de stocker notre base d'apprentissage
+# formaté correctement pour keras afin de ne pas charger les données en mémoire vive
+tmpFile = "tmp.hdf5"
+hdf5Tmp = h5py.File(tmpFile, "w")
+hdf5Tmp.create_dataset("examples", (totalFrames, nbVal * nbCoef)) # dataset données
+hdf5Tmp.create_dataset("languages", (totalFrames,))               # dataset étiquettes
+X = hdf5Tmp.get("examples")
+Y = hdf5Tmp.get("languages")
+
+frameIndex = 0
+# Parcours des datasets du fichier d'entrée afin de remplir les datasets temporaires
 for dataset in hdf5In.values():
-    frames = int((dataset.shape[0]-31)/10)+1
-    totalFrames+=frames
+    print(dataset)
+    # On prend chaque fenêtre de parole du dataset courant
+    for frameStartIndex in range(0, len(dataset) - nbVal, shift):
+        # Une fenêtre de nbVal vecteurs
+        frameVectors = dataset[frameStartIndex : frameStartIndex + nbVal]
+        # On prend le langage sur le premier vecteur de la fenêtre (dernière valeur du vecteur)
+        language = frameVectors[0][len(frameVectors[0])-1]
+        vectorIndex = 0
+        # On va stocker les valeurs de la fenêtre de parole dans un tableau numpy temporaire
+        # cela permet d'accélerer le remplissage des datasets temporaires
+        frameValues = np.empty([nbVal * nbCoef])
+        # Pour chaque vecteur (contenant nbCoef valeurs) de la fenêtre courante
+        for vector in frameVectors :
+            # On remplit le tableau de la fenêtre de parole avec les coefficients du vecteur
+            frameValues[vectorIndex : vectorIndex + nbCoef] = vector[0 : nbCoef]
+            vectorIndex += 13  # au fur et à mesure
 
+        # On remplit le dataset data avec les coefficients du vecteur
+        X[frameIndex] = frameValues
+        Y[frameIndex] = language
+        frameIndex += 1
 
+hdf5In.close()
+        
+# On met les étiquettes sous forme de vecteurs binaires pour keras
+Y = to_categorical(Y)
 
-
-totalFrames *= 20
-print(totalFrames)
-
-
-
-hdf5Out.create_dataset("examples", (totalFrames, 13*31))
-hdf5Out.create_dataset("languages", (totalFrames,))
-
-examplesArray = hdf5Out.get("examples")
-languagesArray = hdf5Out.get("languages")
-
-debut = True
-#examplesArray = np.empty([totalFrames, 13*31])
-#languagesArray = np.empty([totalFrames])
-interval = 31
-shift = 10
-count = 0
-
-#Pour chaque dataset du fichier hdf5
-for j in range(20):
-    print(j)
-    for dataset in hdf5In.values():
-        #print(dataset)
-
-
-        for index in range(0, len(dataset) - interval, shift):
-            values = dataset[index:index+interval]
-            language = values[0][13] #13 en dur
-            datasetValues = np.empty([13*31])
-            count0=0
-            for i in values :
-                #datasetValues = np.concatenate((datasetValues, i[0:13]), axis=0)
-                datasetValues[count0:count0+13] = i[0:13]
-                count0 += 13
-
-            examplesArray[count] = datasetValues
-            languagesArray[count] = language
-            count+=1
-
-languagesArray = to_categorical(languagesArray)
-
-#On crée le modèle séquentiel, avec 4 couches
+# On crée le modèle séquentiel, avec 4 couches
+inputShape = nbVal * nbCoef
 model = Sequential()
-#model.add(Dropout(0.2, input_shape=(13,)))
-model.add(Dense(403, input_shape=(403,), kernel_initializer='normal', activation='relu'))
+model.add(Dropout(0.2, input_shape=(inputShape,)))
+model.add(Dense(inputShape, kernel_initializer='normal', activation='relu'))
 model.add(Dense(256, kernel_initializer='normal', activation='relu'))
 model.add(Dense(256, kernel_initializer='normal', activation='relu'))
 model.add(Dense(4, kernel_initializer='normal', activation='softmax'))
 
-#On compile le modèle
+# On compile le modèle
 model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
 
-#On entraîne le modèle
-model.fit(examplesArray, languagesArray, epochs=10, batch_size=128, shuffle='batch')
+# On entraîne le modèle
+model.fit(X, Y, epochs=10, batch_size=128, shuffle='batch')
 
-#On évalue le modèle
+hdf5Tmp.close() # On peut fermer le fichier temporaire
+remove(tmpFile) # et le supprimer
+
+# On évalue le modèle
 #score = model.evaluate(X, Y)[1]
 #print("Accuracy : " + score)
