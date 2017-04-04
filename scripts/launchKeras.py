3f1f8c5cf457d5ed7c72c7faa97e15443492a339
Zhephyr54 <ale.lanoix@gmail.com>
2017-04-04 12:40:01 +0200

    launchKeras

--- scripts/launchKeras.py
+++ scripts/launchKeras.py
@@ -11,6 +11,8 @@ import sys
 dataFile = sys.argv[1]
 hdf5In = h5py.File(dataFile, "r")
 
+#création d'un fichier hdf5 temporaire
+hdf5Out = h5py.File("tmp.hdf5", "w")
 
 #Nombre d'exemples
 totalFrames = 0
@@ -18,36 +20,48 @@ for dataset in hdf5In.values():
     frames = int((dataset.shape[0]-31)/10)+1
     totalFrames+=frames
 
+totalFrames *= 100
+print(totalFrames)
+
+hdf5Out.create_dataset("examples", (totalFrames, 13*31))
+hdf5Out.create_dataset("languages", (totalFrames,))
+examplesArray = hdf5Out.get("examples")
+languagesArray = hdf5Out.get("languages")
+
 debut = True
-examplesArray = np.empty([totalFrames, 13*31])
-languagesArray = np.empty([totalFrames])
+#examplesArray = np.empty([totalFrames, 13*31])
+#languagesArray = np.empty([totalFrames])
 interval = 31
 shift = 10
 count = 0
 
 #Pour chaque dataset du fichier hdf5
-for dataset in hdf5In.values():
-    print(dataset)
+for i in range(100):
+    for dataset in hdf5In.values():
+        print(dataset)
 
-    for index in range(0, len(dataset) - interval, shift):
-        #if index%1000 == 0 : print(index)
-        values = dataset[index:index+interval]
-        language = values[0][13] #13 en dur
-        datasetValues = []
-        for i in values :
-            datasetValues = np.concatenate((datasetValues, i[0:13]), axis=0)
+        for index in range(0, len(dataset) - interval, shift):
+            #if index%1000 == 0 : print(index)
+            values = dataset[index:index+interval]
+            language = values[0][13] #13 en dur
+            datasetValues = np.empty([13*31])
+            count0=0
+            for i in values :
+                #datasetValues = np.concatenate((datasetValues, i[0:13]), axis=0)
+                datasetValues[count0:count0+13] = i[0:13]
+                count0 += 13
 
-        '''if debut :
-            examplesArray = np.array([datasetValues])
-            languagesArray = np.array([language])
-            debut = False
-        else :
-            examplesArray = np.append(examplesArray, [datasetValues], axis=0)
-            languagesArray = np.append(languagesArray, [language], axis=0)
-           '''
-        examplesArray[count] = datasetValues
-        languagesArray[count] = language
-        count+=1
+            '''if debut :
+                examplesArray = np.array([datasetValues])
+                languagesArray = np.array([language])
+                debut = False
+            else :
+                examplesArray = np.append(examplesArray, [datasetValues], axis=0)
+                languagesArray = np.append(languagesArray, [language], axis=0)
+               '''
+            examplesArray[count] = datasetValues
+            languagesArray[count] = language
+            count+=1
 
 languagesArray = to_categorical(languagesArray)
 
@@ -63,7 +77,7 @@ model.add(Dense(4, kernel_initializer='normal', activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
 
 #On entraîne le modèle
-model.fit(examplesArray, languagesArray, epochs=10, batch_size=128)
+model.fit(examplesArray, languagesArray, epochs=10, batch_size=128, shuffle='batch')
 
 #On évalue le modèle
 #score = model.evaluate(X, Y)[1]
