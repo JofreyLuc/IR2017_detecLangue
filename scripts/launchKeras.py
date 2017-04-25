50b05972f079dd81773d8647d9628cbf1db796f2
Zhephyr54 <ale.lanoix@gmail.com>
2017-04-25 15:23:07 +0200

    Essai du modele avec division par la variance de chaque coeff mfcc et resultats

--- scripts/launchKeras.py
+++ scripts/launchKeras.py
@@ -15,11 +15,83 @@ AFFICHAGE = True
 # Nombre de coefficients cepstraux
 nbCoef = 13
 # Nombre de valeurs à prélever pour obtenir une "fenêtre de parole"
-nbVal = 101
+nbVal = 31
 # Décalage entre chaque prélevement de fenêtre de parole
-shift = 1
+shift = 10
 #Nombre de langages différents = de neurones en sortie
 nbSorties = 4
+
+#Formatte les données en le mettant dans un tableau hdf5 temporaire (utile pour les fichiers dépassant la taille de la ram)
+def formatDataHdf5(dataFileName, hdf5Tmp, withoutLabel=False):
+    '''
+    Formatte les données d'un fichier hdf5 qui est organisé en plusieurs datasets
+    qui contiennent chacun des fenêtres de parole en deux tableaux numpy (données et étiquettes)
+    lisibles par Keras.
+    :param dataFile: Le fichier hdf5 d'entrée.
+    :param hdf5Tmp: Le fichier hdf5 contenant les donnée structurées
+    :param nbVal: Le nombre de valeurs à prélever pour obtenir une "fenêtre de parole"
+    :param nbCoef: Les nombre de coefficient par vecteur acoustique
+    :param shift: Le décalage entre chaque fenêtre de parole
+    :param withoutLabel:Permet de spécifier que les données ne sont pas étiquettées 
+    '''
+    hdf5In = h5py.File(dataFileName, "r")
+    
+    # Calcul du nombre total de fenêtres de parole de la base d'apprentissage
+    # Une fenêtre est une matrice contenant des coefficients cepstraux :
+    # - de nbVal lignes 
+    # - de nbCoef colonnes
+    totalFrames = 0
+    for dataset in hdf5In.values():                             # Pour chaque dataset du fichier hdf5
+        frames = int ((dataset.shape[0] - nbVal) / shift) + 1   # Nb fenêtres du dataset
+        totalFrames += frames
+    
+    # On places les datasets générés par cette fonction dans des groupes nommés de 1 à n
+    groupLastName = 0
+    for groupName in hdf5Tmp:
+        groupLastName = groupName
+    newGroupName = str(int(groupLastName) + 1)
+    # Création des datasets contenant les données formatées
+    hdf5Tmp.create_dataset(newGroupName+"/examples", (totalFrames, nbVal * nbCoef)) # dataset données
+    X = hdf5Tmp.get(newGroupName+"/examples")
+    if not withoutLabel:
+        hdf5Tmp.create_dataset(newGroupName+"/languages", (totalFrames, 4))           # dataset étiquettes
+        Y = hdf5Tmp.get(newGroupName+"/languages")
+
+    # On va stocker les valeurs de la fenêtre de parole dans un tableau numpy temporaire
+    # cela permet d'accélerer le remplissage des datasets temporaires
+    frameValues = np.zeros([nbVal * nbCoef])
+    frameIndex = 0
+    # Parcours des datasets du fichier d'entrée afin de remplir les datasets temporaires
+    for dataset in hdf5In.values():
+        print(dataset)
+        # On prend chaque fenêtre de parole du dataset courant
+        for frameStartIndex in range(0, len(dataset) - nbVal, shift):
+            # Une fenêtre de nbVal vecteurs
+            frameVectors = dataset[frameStartIndex : frameStartIndex + nbVal]
+            # On prend le langage sur le premier vecteur de la fenêtre (dernière valeur du vecteur)
+            if not withoutLabel:
+                language = frameVectors[0][len(frameVectors[0])-1]
+            vectorIndex = 0
+            # Pour chaque vecteur (contenant nbCoef valeurs) de la fenêtre courante
+            for vector in frameVectors :
+                # On remplit le tableau de la fenêtre de parole avec les coefficients du vecteur
+                frameValues[vectorIndex : vectorIndex + nbCoef] = vector[0 : nbCoef]
+                vectorIndex += nbCoef  # au fur et à mesure
+
+            # On remplit le dataset data avec les coefficients du vecteur
+            X[frameIndex] = frameValues
+            if not withoutLabel:
+                tabTemp = np.zeros([4])
+                tabTemp[int(language)] = 1
+                Y[frameIndex] = tabTemp
+            frameIndex += 1
+
+    hdf5In.close()
+    
+    if withoutLabel:
+        return X
+    else:
+        return (X, Y)
 
 
 def formatDataMemory(dataFileName, withoutLabel=False):
@@ -117,17 +189,49 @@ def generatePredict(model, predictFolder, language):
         Xtest = formatDataMemory(i)[0]
         proba = model.predict_proba(Xtest, batch_size=128, verbose=0)
         writeProbaToFile("probas.txt", path.splitext(i)[0], language, proba, langs)
-            
+
+# temp  : pour calculer la variance de chaque coeff mfcc 
+def calculateVariances(dataFileName):
+    hdf5In = h5py.File(dataFileName, "r")
+    
+    nbValue = 0
+    averages = np.zeros([nbCoef], dtype=np.float32)
+    for dataset in hdf5In.values():                             # Pour chaque dataset du fichier hdf5
+        print(dataset)
+        nbValue += dataset.shape[0]
+        for mfccArray in dataset:
+            averages += mfccArray[:nbCoef]
+    averages /= nbValue  # calcul moyenne
+    
+    variances = np.zeros([nbCoef], dtype=np.float32)
+    for dataset in hdf5In.values():                             # Pour chaque dataset du fichier hdf5
+        print(dataset)
+        for mfccArray in dataset:
+            variances += (mfccArray[:nbCoef] - averages)**2
+    variances /= nbValue
+    
+    return variances
+        
 if __name__ == '__main__':
     # On récupère les données
     dataFileName = sys.argv[1]
     devFileName = sys.argv[2]
     testFileName = sys.argv[3]
-
+    
     # On formatte les données
     (X, Y) = formatDataMemory(dataFileName)
     (Xdev, Ydev) = formatDataMemory(devFileName)
     (Xtest, Ytest) = formatDataMemory(testFileName)
+
+    # On calcule la variance de chaque coefficient mfcc
+    # variances obtenues à l'aide de la fonction calculateVariances
+    variances = [ 8.44184971,  3.82373691,  2.00800991,  1.17921674,  1.13604367,  0.72418207,  0.55174363,  0.44808063,  0.40456247,  0.30078402,  0.27061722,  0.24347636, 0.05386801]#calculateVariances(dataFileName)
+    # On concatène nbVal fois le tableau de variances afin de pouvoir effectuer la division en une seule fois
+    variancesDuplicate = []
+    for i in range(nbVal): variancesDuplicate[i*nbVal:] = variances
+    # et on divise chaque coefficient mfcc du train et du dev par sa variance
+    X /= variancesDuplicate
+    Xdev /= variancesDuplicate
 
     # On crée le modèle séquentiel, avec 4 couches
     inputShape = nbVal * nbCoef
@@ -135,8 +239,8 @@ if __name__ == '__main__':
     model.add(Dense(inputShape, input_shape=(inputShape,), kernel_initializer='glorot_normal', activation='relu'))
     model.add(Dropout(0.2, input_shape=(inputShape,)))
     model.add(Dense(256, kernel_initializer='glorot_normal', activation='relu'))
-    model.add(Dropout(0.2, input_shape=(inputShape,)))
-    model.add(Dense(256, kernel_initializer='glorot_normal', activation='relu'))
+    #model.add(Dropout(0.2, input_shape=(inputShape,)))
+    #model.add(Dense(256, kernel_initializer='glorot_normal', activation='relu'))
     model.add(Dense(nbSorties, kernel_initializer='glorot_normal', activation='softmax'))
 
     # On compile le modèle
@@ -151,83 +255,10 @@ if __name__ == '__main__':
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'dev'], loc='upper left')
-    plt.savefig('bar2.png')
+    plt.savefig('testAvecDivVariance.png')
     
     generatePredict(model, 'hdf5Predict/Arabic', 'Arabic')
     generatePredict(model, 'hdf5Predict/English', 'English')
     generatePredict(model, 'hdf5Predict/French', 'French')
     generatePredict(model, 'hdf5Predict/German', 'German')
-
-
     
-#Formatte les données en le mettant dans un tableau hdf5 temporaire (utile pour les fichiers dépassant la taille de la ram)
-def formatDataHdf5(dataFileName, hdf5Tmp, withoutLabel=False):
-    '''
-    Formatte les données d'un fichier hdf5 qui est organisé en plusieurs datasets
-    qui contiennent chacun des fenêtres de parole en deux tableaux numpy (données et étiquettes)
-    lisibles par Keras.
-    :param dataFile: Le fichier hdf5 d'entrée.
-    :param hdf5Tmp: Le fichier hdf5 contenant les donnée structurées
-    :param nbVal: Le nombre de valeurs à prélever pour obtenir une "fenêtre de parole"
-    :param nbCoef: Les nombre de coefficient par vecteur acoustique
-    :param shift: Le décalage entre chaque fenêtre de parole
-    :param withoutLabel:Permet de spécifier que les données ne sont pas étiquettées 
-    '''
-    hdf5In = h5py.File(dataFileName, "r")
-    
-    # Calcul du nombre total de fenêtres de parole de la base d'apprentissage
-    # Une fenêtre est une matrice contenant des coefficients cepstraux :
-    # - de nbVal lignes 
-    # - de nbCoef colonnes
-    totalFrames = 0
-    for dataset in hdf5In.values():                             # Pour chaque dataset du fichier hdf5
-        frames = int ((dataset.shape[0] - nbVal) / shift) + 1   # Nb fenêtres du dataset
-        totalFrames += frames
-    
-    # On places les datasets générés par cette fonction dans des groupes nommés de 1 à n
-    groupLastName = 0
-    for groupName in hdf5Tmp:
-        groupLastName = groupName
-    newGroupName = str(int(groupLastName) + 1)
-    # Création des datasets contenant les données formatées
-    hdf5Tmp.create_dataset(newGroupName+"/examples", (totalFrames, nbVal * nbCoef)) # dataset données
-    X = hdf5Tmp.get(newGroupName+"/examples")
-    if not withoutLabel:
-        hdf5Tmp.create_dataset(newGroupName+"/languages", (totalFrames, 4))           # dataset étiquettes
-        Y = hdf5Tmp.get(newGroupName+"/languages")
-
-    # On va stocker les valeurs de la fenêtre de parole dans un tableau numpy temporaire
-    # cela permet d'accélerer le remplissage des datasets temporaires
-    frameValues = np.zeros([nbVal * nbCoef])
-    frameIndex = 0
-    # Parcours des datasets du fichier d'entrée afin de remplir les datasets temporaires
-    for dataset in hdf5In.values():
-        print(dataset)
-        # On prend chaque fenêtre de parole du dataset courant
-        for frameStartIndex in range(0, len(dataset) - nbVal, shift):
-            # Une fenêtre de nbVal vecteurs
-            frameVectors = dataset[frameStartIndex : frameStartIndex + nbVal]
-            # On prend le langage sur le premier vecteur de la fenêtre (dernière valeur du vecteur)
-            if not withoutLabel:
-                language = frameVectors[0][len(frameVectors[0])-1]
-            vectorIndex = 0
-            # Pour chaque vecteur (contenant nbCoef valeurs) de la fenêtre courante
-            for vector in frameVectors :
-                # On remplit le tableau de la fenêtre de parole avec les coefficients du vecteur
-                frameValues[vectorIndex : vectorIndex + nbCoef] = vector[0 : nbCoef]
-                vectorIndex += nbCoef  # au fur et à mesure
-
-            # On remplit le dataset data avec les coefficients du vecteur
-            X[frameIndex] = frameValues
-            if not withoutLabel:
-                tabTemp = np.zeros([4])
-                tabTemp[int(language)] = 1
-                Y[frameIndex] = tabTemp
-            frameIndex += 1
-
-    hdf5In.close()
-    
-    if withoutLabel:
-        return X
-    else:
-        return (X, Y)
